d68845429e46bb4d25d5e076b7050ca0a84f984b
OksanaLynnyk <allimpiya@gmail.com>
2026-03-30 22:53:09 +0000

    Center Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,15 @@
 import { InfinitySpin } from "react-loader-spinner";
 
+import styles from "./Loader.module.css";
+
 export const Loader = () => (
-  <InfinitySpin
-    visible={true}
-    width="200"
-    color="#f769ca"
-    ariaLabel="infinity-spin-loading"
-    align-self="center"
-  />
+  <div className={styles.loaderWrapper}>
+    <InfinitySpin
+      visible={true}
+      width="300"
+      color="#f769ca"
+      ariaLabel="infinity-spin-loading"
+      align-self="center"
+    />
+  </div>
 );
